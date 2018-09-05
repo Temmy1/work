0eaa95e1375c27f3fbd9c63edf41cf5a8cc200ca
Artyom R <temmyriz@gmail.com>
2018-09-05 17:52:08 +0300

    4th work commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+  return false;
+});
+});
