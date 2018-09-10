b875b486265521f5b629db039ae946847a3e6920
Artyom R <temmyriz@gmail.com>
2018-09-10 17:35:56 +0300

    5th work commit try to toggle panel buttons

--- js/index.js
+++ js/index.js
@@ -1,17 +1,21 @@
 $(document).ready(function(){
-var btn = $('#button');
+  var btn = $('#button');
 
-$(window).scroll(function() {
-  if ($(this).scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
-});
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 300) {
+      btn.addClass('show');
+    } else {
+      btn.removeClass('show');
+    }
+  });
 
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
-  return false;
-});
+  btn.on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+    return false;
+  });
+
+  $("#panel1").hover(function(){
+    $(".panel-btn").toggle();
+  });
 });
